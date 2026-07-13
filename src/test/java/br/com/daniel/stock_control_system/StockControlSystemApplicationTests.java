96334ca0b6aa90507a3f092571ee3b36b4f49054
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-13 18:32:04 -0300

    chore: estrutura inicial do projeto

---
+++ src/test/java/br/com/daniel/stock_control_system/StockControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daniel.stock_control_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
