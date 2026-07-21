3e9cf5fa36a18ec8e9e5e3469c77126b519bc08e
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-21 00:08:47 -0300

    adicionando na CategoriaRequest as validacoes do formulario categoria

--- src/main/java/br/com/daniel/stock_control_system/dto/request/CategoriaRequest.java
+++ src/main/java/br/com/daniel/stock_control_system/dto/request/CategoriaRequest.java
@@ -1,7 +1,14 @@
 package br.com.daniel.stock_control_system.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CategoriaRequest(
+        @NotBlank
+        @Size(min = 3, max = 100)
         String nome,
+
+        @Size(max = 255)
         String descricao
 ) {
 
