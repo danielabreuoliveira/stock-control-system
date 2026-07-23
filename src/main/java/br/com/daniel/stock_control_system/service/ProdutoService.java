a9e247b8375588dbf51e45d95abbb48675155213
Daniel de Abreu Oliveira <danieloliveirabsi@gmail.com>
2026-07-23 11:33:53 -0300

    implementando o ProdutoService com metodo salvar

---
+++ src/main/java/br/com/daniel/stock_control_system/service/ProdutoService.java
@@ -0,0 +1,35 @@
+package br.com.daniel.stock_control_system.service;
+
+import br.com.daniel.stock_control_system.dto.request.ProdutoRequest;
+import br.com.daniel.stock_control_system.dto.response.ProdutoResponse;
+import br.com.daniel.stock_control_system.entity.Categoria;
+import br.com.daniel.stock_control_system.entity.Produto;
+import br.com.daniel.stock_control_system.mapper.ProdutoMapper;
+import br.com.daniel.stock_control_system.repository.CategoriaRepository;
+import br.com.daniel.stock_control_system.repository.ProdutoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository;
+    private final CategoriaRepository categoriaRepository;
+
+    public ProdutoService(ProdutoRepository produtoRepository, CategoriaRepository categoriaRepository){
+        this.produtoRepository = produtoRepository;
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    public ProdutoResponse salvar (ProdutoRequest request){
+
+        Categoria categoria = categoriaRepository.findById(request.categoriaId()).orElseThrow(() -> new RuntimeException("categoria não encontrada"));
+
+        Produto produto = ProdutoMapper.toEntity(request);
+
+        produto.setCategoria(categoria);
+
+        produtoRepository.save(produto);
+
+        return ProdutoMapper.toResponse(produto);
+    }
+}
